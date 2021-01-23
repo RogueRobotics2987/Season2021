9f413388f497a6ffea4e03a957ff83597f134caa
= <=>
2021-01-23 10:53:08 -0600

    initial commit

---
+++ src/main/cpp/subsystems/DifferentialDriveSubsystem.cpp
@@ -0,0 +1,17 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+#include "subsystems/DifferentialDriveSubsystem.h"
+
+DifferentialDriveSubsystem::DifferentialDriveSubsystem() {
+  // Implementation of subsystem constructor goes here.
+}
+
+void DifferentialDriveSubsystem::Periodic() {
+  // Implementation of subsystem periodic method goes here.
+}
+
+void DifferentialDriveSubsystem::SimulationPeriodic() {
+  // Implementation of subsystem simulation periodic method goes here.
+}
