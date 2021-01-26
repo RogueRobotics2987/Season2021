f43402d2eb0395d3e8104c48a34c4debf7bc27f0
= <=>
2021-01-26 15:38:29 -0600

    Drive scaler output to SmartDashboard

--- src/main/cpp/commands/TankDrive.cpp
+++ src/main/cpp/commands/TankDrive.cpp
@@ -9,6 +9,7 @@
 
 #include "Robot.h"
 
+
 TankDrive::TankDrive(DriveTrain* drivetrain, frc::Joystick* stickRight, frc::Joystick* stickLeft)
     {
   
@@ -17,20 +18,48 @@ TankDrive::TankDrive(DriveTrain* drivetrain, frc::Joystick* stickRight, frc::Joy
   m_stickRight = stickRight; 
   SetName("TankDrive");
   AddRequirements({m_drivetrain});
+  
+  
 }
 
 // Called repeatedly when this Command is scheduled to run
 void TankDrive::Execute() { 
-  if (m_stickLeft->GetRawButton(2)) {
+  
+  static double lastLeft = 0.0; 
+  static double lastRight = 0.0;
+
+  double Left = m_stickLeft -> GetY(); //getting the Y value from the joystick
+  double Right = m_stickRight -> GetX();
+  double outLeft = 0;
+  double outRight = 0;
+  double slope = 0.025; //0.01 was too slow, 0.05 was too fast
+ 
+ frc::SmartDashboard::PutNumber("lastLeft Value", lastLeft);
+ frc::SmartDashboard::PutNumber("Left value", Left);
+
+  if (abs(Left-lastLeft) >slope) {
+    outLeft = lastLeft + copysignf(1.0,Left - lastLeft)*slope;
+    } else {
+      outLeft = Left;
+  }
+  if (abs(Right-lastRight) >slope) {
+    outRight = lastRight + copysignf(1.0,Right - lastRight)*slope;
+    } else {
+      outRight = Right;
+  }
+  
+  m_drivetrain -> Drive(outLeft, outRight);
+   lastLeft = outLeft;
+  lastRight = outRight; 
+  /*if (m_stickLeft->GetRawButton(2)) {
     m_drivetrain->Drive(-m_stickLeft->GetY(), m_stickRight->GetX()); 
   } else {
     m_drivetrain->Drive(m_stickLeft->GetY(), m_stickRight->GetX()); 
-  }
-  
+  } */
   }
 
 // Make this return true when this Command no longer needs to run execute()
 bool TankDrive::IsFinished() { return false; }
 
 // Called once after isFinished returns true
-void TankDrive::End(bool) { m_drivetrain->Drive(0, 0); }
+void TankDrive::End(bool) { m_drivetrain->Drive(0, 0);}
