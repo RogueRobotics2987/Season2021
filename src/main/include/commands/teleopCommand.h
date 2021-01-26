b3f455d21d2be8d07a0097001d66e77fe5cd51ea
=someone2317 <=samherrera2016@icloud.com>
2021-01-26 17:59:29 -0600

    Initial commit

--- src/main/include/commands/teleopCommand.h
+++ src/main/include/commands/teleopCommand.h
@@ -28,4 +28,5 @@ class TeleopCommand
 
  private:
   DifferentialDriveSubsystem* m_subsystem;
+ 
 };
--- src/main/include/subsystems/DifferentialDriveSubsystem.h
+++ src/main/include/subsystems/DifferentialDriveSubsystem.h
@@ -3,36 +3,12 @@
 // the WPILib BSD license file in the root directory of this project.
 
 #pragma once
-#include "AHRS.h"
-#include <frc/AnalogGyro.h>
-#include <frc/AnalogInput.h>
-#include <frc/Encoder.h>
-#include <frc/PWMVictorSPX.h>
-#include <frc/SpeedControllerGroup.h>
-#include <frc/drive/DifferentialDrive.h>
+
 #include <frc2/command/SubsystemBase.h>
-#include "rev/CANSparkMax.h"
-#include <frc/kinematics/DifferentialDriveOdometry.h>
-#include <frc/kinematics/DifferentialDriveWheelSpeeds.h>
-#include <frc/geometry/Rotation2d.h>
-#include <frc/trajectory/Trajectory.h> 
-#include <frc/trajectory/TrajectoryGenerator.h>
-#include <units/units.h>
 #include "ctre/Phoenix.h"
+#include "frc/SpeedControllerGroup.h"
+#include <frc/drive/DifferentialDrive.h>
 
-
-
-
-
-namespace frc {
-class Joystick;
-}  // namespace frc
-
-/**
- * The DriveTrain subsystem incorporates the sensors and actuators attached to
- * the robots chassis. These include four drive motors, a left and right encoder
- * and a gyro.
- */
 class DifferentialDriveSubsystem : public frc2::SubsystemBase {
  public:
   DifferentialDriveSubsystem();
@@ -40,58 +16,8 @@ class DifferentialDriveSubsystem : public frc2::SubsystemBase {
   /**
    * The log method puts interesting information to the SmartDashboard.
    */
-  void Log();
-  void Periodic(); 
-  void autonDrive(); 
-  /**
-   * Tank style driving for the DriveTrain.
-   * @param left Speed in range [-1,1]
-   * @param right Speed in range [-1,1]
-   */
-  void Drive(double y, double z);
 
-  /**
-   * @return The robots heading in degrees.
-   */
-  double GetHeading();
-  void ResetOdometry(frc::Pose2d pose); 
-
-  /**
-   * Reset the robots sensors to the zero states.
-   */
-  void Reset();
-
-  /**
-   * @return The distance driven (average of left and right encoders).
-   */
-  double GetDistance();
-
-  /**
-   * @return The distance to the obstacle detected by the rangefinder.
-   */
-  double GetDistanceToObstacle();
-  double GetTurnRate(); 
-
-frc::Pose2d GetPose(); 
-void ResetEncoders(); 
-void TrajectoryInit(); 
-frc::DifferentialDriveWheelSpeeds GetWheelSpeeds(); 
-void TankDriveVolts(units::volt_t left, units::volt_t right); 
-
-  
-
- private:
-
-  WPI_TalonSRX* Motor;
-  WPI_TalonSRX* Motor2;
-  WPI_TalonSRX* Motor3;
-  WPI_TalonSRX* Motor4;
-  frc::DifferentialDrive* DriverFront;
-  frc::DifferentialDrive* DriverBack;
-  frc::DifferentialDrive* m_robotDrive;
-  AHRS* myAhrs; 
-  frc::DifferentialDriveOdometry* m_odometry;
-
+   void Periodic() override;
 
   /**
    * Will be called periodically whenever the CommandScheduler runs during
@@ -100,6 +26,15 @@ void TankDriveVolts(units::volt_t left, units::volt_t right);
   void SimulationPeriodic() override;
 
  private:
+ WPI_TalonSRX FrontLeft = {12};
+ WPI_TalonSRX FrontRight = {16};
+ WPI_TalonSRX BackLeft = {14};
+ WPI_TalonSRX BackRight = {15};
+ frc::SpeedControllerGroup m_leftMoters{FrontLeft, BackLeft};
+
+ frc::SpeedControllerGroup m_rightMoters{BackRight, FrontRight};
+
+ frc::DifferentialDrive m_drive{m_leftMoters, m_rightMoters};
   // Components (e.g. motor controllers and sensors) should generally be
   // declared private and exposed only through public methods.
 };
