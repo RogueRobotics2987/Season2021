1be9bb74cd35f700f00d547c29e1a5862ea74c4e
= <=>
2021-02-04 15:21:32 -0600

    Just so I can pull the updated code

---
+++ src/main/include/commands/ArmCommand.h
@@ -0,0 +1,35 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+#pragma once
+
+#include <frc2/command/CommandBase.h>
+#include <frc2/command/CommandHelper.h>
+#include <frc/Joystick.h>
+#include "subsystems/ArmSubsystem.h"
+
+/**
+ * An example command that uses an example subsystem.
+ *
+ * <p>Note that this extends CommandHelper, rather extending CommandBase
+ * directly; this is crucially important, or else the decorator functions in
+ * Command will *not* work!
+ */
+class ArmCommand : public frc2::CommandHelper<frc2::CommandBase, ArmCommand>{
+    
+ public:
+  /**
+   * Creates a new DifferentialDriveCommand.
+   *
+   * @param subsystem The subsystem used by this command.
+   */
+  ArmCommand(ArmSubsystem* subsystem, frc::Joystick* xbox);
+  //DifferentialDriveCommand(ArmSubsystem* subsystem, frc::Joystick* xbox);
+  void Execute() override;
+  bool IsFinished() override;
+  void End(bool Interrupted) override;
+ private:
+ ArmSubsystem* m_subsystem;
+frc::Joystick* xbox;
+};
