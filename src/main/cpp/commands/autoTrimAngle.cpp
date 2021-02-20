b2bf8d760bd447013debc9c253911672ceb0b73b
=your-github-username <=>
2021-02-20 13:30:13 -0600

    fix autoTrimAngle

--- src/main/cpp/commands/autoTrimAngle.cpp
+++ src/main/cpp/commands/autoTrimAngle.cpp
@@ -9,20 +9,23 @@ autoTrimAngle::autoTrimAngle(ShooterActuator* a_actuator, bool shootingMode) {
   m_shootingMode = shootingMode;
   AddRequirements(m_actuator);
   myTimer = new frc::Timer;
-  myTimer -> Reset();
-  myTimer -> Start();
-  myTimer -> Get();
   
 }
 
 // Called when the command is initially scheduled.
-void autoTrimAngle::Initialize() {}
+void autoTrimAngle::Initialize() {
+  myTimer -> Start();
+  myTimer -> Reset();
+}
 
 // Called repeatedly when this Command is scheduled to run
 void autoTrimAngle::Execute() {
   currTime = myTimer -> Get();
 
   if (currTime <= 5 && m_shootingMode == true){
+    m_actuator -> limeStream(2);
+    frc::SmartDashboard::PutNumber("Set RPM", 3950);
+
     m_actuator->SetAutoAim(m_shootingMode);
   } else {
     m_shootingMode = false;
