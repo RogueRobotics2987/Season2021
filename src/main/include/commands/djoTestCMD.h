5dd25a89b9d717b34df67331f9e876faa88e1446
= <=>
2021-02-22 13:53:07 -0600

    initial commit

---
+++ src/main/include/commands/djoTestCMD.h
@@ -0,0 +1,18 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+#pragma once
+
+#include <frc2/command/CommandHelper.h>
+#include <frc2/command/SequentialCommandGroup.h>
+#include "subsystems/DriveSubsystem.h"
+#include "commands/TurnToAngle.h"
+
+class djoTestCMD
+    : public frc2::CommandHelper<frc2::SequentialCommandGroup, djoTestCMD> {
+    //: public frc2::CommandHelper<frc2::ParallelCommandGroup, djoTestCMD> {
+      
+ public:
+  djoTestCMD(DriveSubsystem &m_drive);
+};
