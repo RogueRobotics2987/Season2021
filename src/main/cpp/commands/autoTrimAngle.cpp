5f43daab3bd383667fbc92433fbf26ba8ee7ea2c
=your-github-username <=>
2021-02-25 16:14:49 -0600

    test

--- src/main/cpp/commands/autoTrimAngle.cpp
+++ src/main/cpp/commands/autoTrimAngle.cpp
@@ -2,7 +2,7 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-#include "commands/autoTrimAngle.h"
+#include "commands/AutoTrimAngle.h"
 
 autoTrimAngle::autoTrimAngle(ShooterActuator* a_actuator, bool shootingMode) {
   m_actuator = a_actuator;
