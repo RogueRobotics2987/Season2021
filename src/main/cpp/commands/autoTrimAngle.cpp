739fecab20b29ad120b06b152f91351d1d0d5a6e
= <=>
2021-02-27 10:30:39 -0600

    Dannalynn's non working AutoTrimAngle

--- src/main/cpp/commands/autoTrimAngle.cpp
+++ src/main/cpp/commands/autoTrimAngle.cpp
@@ -2,43 +2,47 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-#include "commands/autoTrimAngle.h"
+#include "commands/AutoTrimAngle.h"
 
-autoTrimAngle::autoTrimAngle(ShooterActuator* a_actuator, bool shootingMode) {
-  m_actuator = a_actuator;
-  m_shootingMode = shootingMode;
+AutoTrimAngle::AutoTrimAngle(ShooterActuator* a_actuator, bool shootingMode) {
+  // Use addRequirements() here to declare subsystem dependencies.
+  m_actuator = a_actuator; 
+  m_shootingMode = shootingMode; 
   AddRequirements(m_actuator);
   myTimer = new frc::Timer;
-  myTimer -> Reset();
-  myTimer -> Start();
-  myTimer -> Get();
   
 }
 
 // Called when the command is initially scheduled.
-void autoTrimAngle::Initialize() {}
+void AutoTrimAngle::Initialize() {
+  myTimer -> Reset();
+  myTimer -> Start();
+}
 
 // Called repeatedly when this Command is scheduled to run
-void autoTrimAngle::Execute() {
-  currTime = myTimer -> Get();
+void AutoTrimAngle::Execute() {
+    curTime = myTimer -> Get();
 
-  if (currTime <= 5 && m_shootingMode == true){
-    m_actuator->SetAutoAim(m_shootingMode);
+  if (curTime <= 45 && m_shootingMode == true){
+    m_actuator -> limeStream(2); 
+      frc::SmartDashboard::PutNumber("Set RPM", 3950); 
+    m_actuator->SetAutoAim(m_shootingMode); 
+
   } else {
     m_shootingMode = false;
-  }
+  } 
 }
 
 // Called once the command ends or is interrupted.
-void autoTrimAngle::End(bool interrupted) {}
+void AutoTrimAngle::End(bool interrupted) {}
 
 // Returns true when the command should end.
-bool autoTrimAngle::IsFinished() {
-  currTime = myTimer -> Get();
-  
-  if (currTime > 5){
+bool AutoTrimAngle::IsFinished() {
+     curTime = myTimer -> Get();
+
+  if (curTime>= 45) {
     return true;
-  } else { 
+  } else {
     return false;
   }
 }
