52db12f99e17e8d109b58e0c6858e1704312fdf1
=Knudsen231 <=sledracen@gmail.com>
2021-03-19 16:46:05 -0500

    Created Actuator.cpp Actuatot.h

---
+++ src/main/cpp/commands/Actuator.cpp
@@ -0,0 +1,38 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+#include "commands/Actuator.h"
+
+Actuator::Actuator(ActuatorSubsystem* m_Actuator):m_Acuator(m_Actuator) {
+  AddRequirements(m_Actuator);
+  // Use addRequirements() here to declare subsystem dependencies.
+}
+
+// Called when the command is initially scheduled.
+void Actuator::Initialize() {
+  
+}
+
+
+
+// Called repeatedly when this Command is scheduled to run
+void Actuator::Execute() {
+
+ 
+}
+
+// Called once the command ends or is interrupted.
+void Actuator::End(bool interrupted) {
+}
+
+// Returns true when the command should end.
+bool Actuator::IsFinished() { 
+  return false;
+    
+    
+
+ }
